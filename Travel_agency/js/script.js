650e829683b12fa084a274c1adc2a2893a5ee22a
parishram756 <sulabspakota1@gmai.com>
2025-08-14 12:24:21 +0100

    sulav

--- Travel_agency/js/script.js
+++ Travel_agency/js/script.js
@@ -37,19 +37,3 @@ $(document).ready(function() {
 });
 
 
-function toggleFavorite(iconElement) {
-            iconElement.classList.toggle('favorited');
-        }
-
-        // Initialize all carousels on the page
-        document.addEventListener('DOMContentLoaded', function () {
-            // Select all elements with the class 'carousel'
-            const carousels = document.querySelectorAll('.carousel');
-            // Loop through each carousel and initialize it as a Bootstrap carousel
-            carousels.forEach(function (carousel) {
-                new bootstrap.Carousel(carousel, {
-                    interval: 3000, // Change slide every 3 seconds
-                    wrap: true // Continue sliding from the last item to the first
-                });
-            });
-        });
